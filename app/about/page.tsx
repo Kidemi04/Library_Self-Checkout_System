b663eea0456864faf80996868bb3e141b4207382
print1Username <128358949+print1Username@users.noreply.github.com>
2025-09-30 22:08:31 +0800

    change little

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function AboutPage() {
+    return (
+        <main>
+            <title>About</title>
+        </main>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col bg-slate-50">
       {/* Header / Banner */}
+      <title>Main Page</title>
       <header className="flex items-center justify-between bg-blue-600 px-6 py-4">
         <img
           className="h-10 md:h-14"
