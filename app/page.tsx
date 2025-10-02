1c87395bd88b2936c51aff2b29d0c920a24638de
print1Username <128358949+print1Username@users.noreply.github.com>
2025-10-02 10:49:53 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -50,6 +50,7 @@ export default function HomePage() {
                 </Link>
                 <Link
                     href="https://www.swinburne.edu.my/"
+                    target="_blank"
                     className="rounded-lg border border-swin-charcoal/20 px-6 py-3 text-swin-charcoal hover:border-swin-red hover:bg-swin-red hover:text-swin-ivory" >
                 Learn More
                 </Link>
